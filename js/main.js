47efc4481ab440b4ed38e242664f4482561a1857
Tasnimul Hasan Choyon <156509477+tasnimulchoyon@users.noreply.github.com>
2024-11-22 22:29:05 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+document.write("Shikhbe Shobai");
+document.write("Hlw"); 
+document.write("9+10");
+document.write("next");
+document.write(10+10);
+document.write("break");
+
+document.write("10+5=" +(10+5));
+document.write("break");
+
+document.write("20-7=" + (20-7));"<br>"
+document.write("break");
+
+console.log(10+5);
+console.log("10+10");
+
+window.alert("This website collect your information !");
+
+
+var pera = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat culpa aperiam autem tempora provident natus minima blanditiis molestiae incidunt. Perferendis, voluptas adipisci saepe suscipit nihil neque incidunt? Expedita, alias maxime.";
+
+console.log(pera);
+document.write(pera);
+
+
+document.write("break");
+document.write("break");
+document.write("......................");
+
+var fastname = "Omuk";
+var deg = "Web dev";
+var responsibility = "Congratulatons Mr " + fastname + ". you are promoted to senior" + deg + ". your responsibility is .......<br> thankyou & best wishes Mr. " +  fastname + "."
+
+document.write(responsibility);
+
+
+
+var x = 25;
+var y = 15;
+var plus = x+y;
+
+document.write(plus);
+document.write("......................");
+
+var m = 15;
+var n = 35;
+var together = m+n;
+
+document.write ("15+35 = " + together)
+
+
